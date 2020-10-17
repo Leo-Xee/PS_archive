8996a5fa1adb73caad6065c68f9d67f58bc5b3e5
이장민 <jang.xmin@gmail.com>
2020-10-17 23:21:16 +0900

    DP1

---
+++ BaekJoon/Boj_1463_topDown.cpp
@@ -0,0 +1,43 @@
+/*
+ * 1로 만들기 (1463)
+ * 
+ * Top down 방식
+ * D[N] = N을 1로 만들 수 있는 최소 연산 횟수
+ * D[N] = D[N/3] + 1 or D[N/2] + 1 or D[N-1] + 1
+ *      = min(D[N/3], D[N/2], D[N-1]) + 1 이다.
+ */
+
+#include <iostream>
+using namespace std;
+
+// 1 <= N <= 10^6
+int d[100001];
+int go(int n) {
+    if (n == 1) {
+        return 0;
+    }
+    if (d[n] > 0) {
+        return d[n];
+    }
+    d[n] = go(n-1) + 1;
+    if (n%2 == 0) {
+        int temp = go(n/2) + 1;
+        if (d[n] > temp) {
+            d[n] = temp;
+        }
+    }
+    if (n%3 == 0) {
+        int temp = go(n/3) + 1;
+        if (d[n] > temp) {
+            d[n] = temp;
+        }
+    }
+    return d[n];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << go(n) << '\n';
+    return 0;
+}
